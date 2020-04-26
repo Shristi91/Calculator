4b759b657e0372aaf6f083750498c2602db0cc41
Shristi91 <63180945+Shristi91@users.noreply.github.com>
2020-04-26 23:08:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,13 +3,14 @@ const calculatorScreen = document.querySelector(".calculator_screen")
 let currentInput = '0'
 let calculationOperator = ' '
 let prevInput = '0'
-
 const updateScreen = (number) =>{
       calculatorScreen.value = number
 }
-const inputNumber = (number) => {
+const inputNumber = (number) =>{
       currentInput += number
 }
+
+
 numbers.forEach((number) =>{
     number.addEventListener("click", (event) =>{
           inputNumber(event.target.value)
